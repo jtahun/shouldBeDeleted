142d25b8bc82cee29d7ab2b34aade40170b6478f
jtahun <jtahun@bk.ru>
2022-06-15 10:00:05 +0600

    read write to/from file

--- src/gen/com/igorzhuley/Main.java
+++ src/gen/com/igorzhuley/Main.java
@@ -1,21 +1,42 @@
 package gen.com.igorzhuley;
 
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 
 /**
  * Created by Jtahun on 15.06.2022.
  */
+
 public class Main {
   public static void main(String[] args) {
-    try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./myLogFile.txt"), StandardCharsets.UTF_8))) {
-      writer.write(" write log to file");
-    }
-    catch (IOException ex) {
-      ex.printStackTrace();
+    Scanner cin = new Scanner(System.in);
+    try {
+      BufferedReader in = new BufferedReader(new FileReader("./logfile.txt"));
+      String str;
+
+      while ((str = in.readLine()) != null) {
+        System.out.println(str);
+      }
+      System.out.println(str);
+      in.close();
+    } catch (IOException e) {
     }
 
+    try {
+      Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./logFile.txt",true), StandardCharsets.UTF_8));
+      //writer.write("  ");
+      writer.write("\t\n");
+      writer.write((cin.nextLine())); //не допетрил как символ новой строки прикрутить
+
+      writer.close();
+      cin.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+
+    }
   }
 }
+
 
